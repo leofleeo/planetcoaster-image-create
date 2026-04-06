f2dbfc03c86398f77299835b81b3ed4318aa58e0
Leo <83542885+leofleeo@users.noreply.github.com>
2026-04-06 19:29:25 +0000

    remove unused import

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { type Dispatch, FC, type SetStateAction, useEffect, useRef, useState } from "react";
+import { type Dispatch, type SetStateAction, useEffect, useRef, useState } from "react";
 import {
 	TransformComponent,
 	TransformWrapper,
