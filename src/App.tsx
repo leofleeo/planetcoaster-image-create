de82104b54da3a609065dbca1101a4a084dd9e76
Leo <83542885+leofleeo@users.noreply.github.com>
2026-04-24 15:34:56 +0000

    Switch to vite but processing doesn't work

--- src/App.tsx
+++ src/App.tsx
@@ -143,7 +143,7 @@ export default function Home() {
 
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<main className="flex min-h-screen w-full flex-col items-center justify-normal gap-5 py-16 px-16 bg-white dark:bg-black sm:items-start">
+			<main className="flex min-h-screen w-full flex-col items-center justify-normal gap-5 p-8 bg-white dark:bg-black sm:items-start">
 				<div className="flex items-center justify-between w-full">
 					<h1 className="text-2xl font-bold">Planet Coaster Map Creator</h1>
 					<ThemeSwitcher />
