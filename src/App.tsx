5989bc1bb94bc8b7b96c2cbee552ac0e62d833ef
leofleeo <83542885+leofleeo@users.noreply.github.com>
2026-06-10 21:20:48 -0400

    Add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -141,7 +141,16 @@ export default function Home() {
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<main className="flex min-h-screen w-full flex-col items-center justify-normal gap-5 p-8 bg-white dark:bg-black sm:items-start">
 				<div className="flex items-center justify-between w-full">
-					<h1 className="text-2xl font-bold">Planet Coaster Map Creator</h1>
+					<span className="flex items-center gap-2">
+						<img
+							src="/favicon.png"
+							alt="rollercoaster in landscape inside crop icon"
+							className="rounded-md p-2 bg-white"
+							width={48}
+							height={48}
+						/>
+						<h1 className="text-2xl font-bold">Planet Coaster Map Creator</h1>
+					</span>
 					<ThemeSwitcher />
 				</div>
 				<div className="w-full flex gap-4 flex-row">
